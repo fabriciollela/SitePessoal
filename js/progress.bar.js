16f89c567be96ad57d2ca6034a3370445bc2df99
Fabricio Villela <fabricio_villela@hotmail.com>
2019-06-22 15:56:22 -0300

    adicionando progressbar

---
+++ js/progress.bar.js
@@ -0,0 +1,7 @@
+$("#progressBar").click(function(){
+    $('.html').animate({width:'95%'},2000);
+    $('.css').animate({width:'85%'},2000);
+    $('.js').animate({width:'90%'},2000);
+    $('.php').animate({width:'70%'},2000);
+    $('.ajax').animate({width:'30%'},2000);
+});
