d1422ec7ba96374694abb8e6a4cea4f6a004aa41
Fabricio Villela <fabricio_villela@hotmail.com>
2019-06-26 23:29:33 -0300

    finalização de habilidades

--- js/progress.bar.js
+++ js/progress.bar.js
@@ -1,7 +1,58 @@
-$("#progressBar").click(function(){
-    $('.html').animate({width:'95%'},2000);
-    $('.css').animate({width:'85%'},2000);
-    $('.js').animate({width:'90%'},2000);
-    $('.php').animate({width:'70%'},2000);
-    $('.ajax').animate({width:'30%'},2000);
+$(window).scroll(function () {
+    var posicaoTela = $(window).scrollTop(),
+        posicaoReq = $('#analiseRequisitos').offset().top,
+        posicaoTeste = $('#testeSoftware').offset().top,
+        posicaoProgramacao = $('#programacao').offset().top,
+        posicaoReq = posicaoReq - 400,
+        posicaoTeste = posicaoTeste - 400,
+        posicaoProgramacao = posicaoProgramacao -400;
+
+    if (posicaoTela > (posicaoReq)) {
+        $('.levantamento').animate({
+            width: '95%'
+        }, 2000);
+        $('.uc').animate({
+            width: '85%'
+        }, 2000);
+        $('.epe').animate({
+            width: '90%'
+        }, 2000);
+        $('.historiaUsuario').animate({
+            width: '70%'
+        }, 2000);
+        $('.processos').animate({
+            width: '30%'
+        }, 2000);
+        $('.modelagemBanco').animate({
+            width: '18%'
+        }, 2000);
+    }
+    if (posicaoTela > (posicaoTeste)) {
+        $('.docTeste').animate({
+            width: '95%'
+        }, 2000);
+        $('.execTeste').animate({
+            width: '95%'
+        }, 2000);
+        $('.autoTeste').animate({
+            width: '18%'
+        }, 2000);
+    }
+    if (posicaoTela > (posicaoProgramacao)) {
+        $('.frontEnd').animate({
+            width: '40%'
+        }, 2000);
+        $('.java').animate({
+            width: '20%'
+        }, 2000);
+        $('.kotlin').animate({
+            width: '15%'
+        }, 2000);
+        $('.banco').animate({
+            width: '20%'
+        }, 2000);
+        $('.git').animate({
+            width: '80%'
+        }, 2000);
+    }
 });
